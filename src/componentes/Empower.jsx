25c559736aff2d8710227d20cced57d9631f05dd
GaziMaksudur <gazimaksudur2@gmail.com>
2024-07-04 01:33:21 +0600

    empower section is properly done

--- src/componentes/Empower.jsx
+++ src/componentes/Empower.jsx
@@ -2,35 +2,51 @@ import { MdArrowOutward } from "react-icons/md";
 import conversation_img1 from '../assets/Rectangle 27.png'
 import conversation_img2 from '../assets/Rectangle 27-1.png'
 import conversation_img3 from '../assets/Rectangle 27-2.png'
+import up_arrow from '../assets/Group 28.png'
+
 
 const Empower = () => {
     return (
-        <div className='px-16 py-16 mb-24 w-[98%] bg-[#FFFFF5] mx-auto grid grid-cols-2 gap-16 rounded-badge'>
-            <div className='space-y-5 w-[70%]'>
-                <button className='btn btn-outline rounded-full'>Who we are</button>
-                <h2 className='capitalize'>We help to get solutions</h2>
-                <p>We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.</p>
-                <button className='btn btn-warning'>Learn More<MdArrowOutward size={20}/></button>
-            </div>
-            <div className="relative">
-                <img className="rounded-3xl" src={conversation_img3} alt="conversation_img" />
-                <div className="absolute bottom-0">
-                    <h4>Our mission is simple</h4>
-                    <p>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
+        <div className='px-20 py-20 mb-24 w-[98%] space-y-14 bg-[#FFFFF5] mx-auto rounded-badge'>
+            <div className="flex justify-between">
+                <div className='space-y-5 w-[50%]'>
+                    <button className='btn btn-outline rounded-full'>Service</button>
+                    <h2 className='capitalize w-[50%] text-[#020043] text-4xl font-semibold'>Empowering Health, Enriching Lives</h2>
+                    <p className="w-[70%] text-[#020043ab] pb-4">We are committed to providing high-quality, compassionate care to every patient we serve. Whatever your healthcare needs may be, you can trust us to be your partner in health and wellness.</p>
+                    <button className='btn btn-warning'>Appointment<MdArrowOutward size={20} /></button>
+                </div>
+                <div className="relative">
+                    <img className="rounded-3xl" src={conversation_img3} alt="conversation_img" />
+                    <div className="absolute z-[20] space-y-2 left-4 bottom-4 bg-[#020043a0] px-4 py-4 rounded-3xl w-[70%] text-white">
+                        <h4 className="text-xl">Advanced Technology</h4>
+                        <p className="text-gray-400 text-sm">We are committed to providing high-quality, compassionate care to every patient we serve. Whatever your healthcare needs may be, you can trust us to be your partner in health and wellness.</p>
+                        <div className="w-full flex justify-end">
+                            <img src={up_arrow} alt="up arrow" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="relative">
-                <img className="rounded-3xl" src={conversation_img2} alt="conversation_img" />
-                <div className="absolute bottom-0">
-                    <h4>Our mission is simple</h4>
-                    <p>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
+
+            <div className="flex justify-between">
+                <div className="relative">
+                    <img className="rounded-3xl" src={conversation_img2} alt="conversation_img" />
+                    <div className="absolute z-[20] space-y-2 left-4 bottom-4 bg-[#020043a0] px-4 py-4 rounded-3xl w-[70%] text-white">
+                        <h4 className="text-xl">Online Doctor Meet</h4>
+                        <p className="text-gray-400 text-sm">Our surgeons are trained in the latest robotic surgical techniques, which allow for greater precision.</p>
+                        <div className="w-full flex justify-end">
+                            <img src={up_arrow} alt="up arrow" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="relative">
-                <img className="rounded-3xl" src={conversation_img1} alt="conversation_img" />
-                <div className="absolute bottom-0">
-                    <h4>Our mission is simple</h4>
-                    <p>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
+                <div className="relative">
+                    <img className="rounded-3xl" src={conversation_img1} alt="conversation_img" />
+                    <div className="absolute z-[20] space-y-2 left-4 bottom-4 bg-[#020043a0] px-4 py-4 rounded-3xl w-[70%] text-white">
+                        <h4 className="text-xl">Consultancy your health</h4>
+                        <p className="text-gray-400 text-sm">Our surgeons are trained in the latest robotic surgical techniques, which allow for greater precision</p>
+                        <div className="w-full flex justify-end">
+                            <img src={up_arrow} alt="up arrow" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/componentes/OurMission.jsx
+++ src/componentes/OurMission.jsx
@@ -3,18 +3,18 @@ import conversation_img from '../assets/Rectangle 24.png'
 
 const OurMission = () => {
     return (
-        <div className='px-6 py-16 mb-24 w-[98%] mx-auto flex justify-between items-center'>
+        <div className='px-6 py-16 my-24 w-[98%] mx-auto flex justify-between items-center'>
             <div className='space-y-5 w-[38%]'>
                 <button className='btn btn-outline rounded-full'>Who we are</button>
-                <h2 className='capitalize'>We help to get solutions</h2>
-                <p>We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.</p>
+                <h2 className='capitalize w-[50%] text-[#020043] text-4xl font-semibold'>We help to get solutions</h2>
+                <p className="w-[90%] text-[#020043ab] pb-4">We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.</p>
                 <button className='btn btn-warning'>Learn More<MdArrowOutward size={20}/></button>
             </div>
             <div className="relative">
                 <img className="" src={conversation_img} alt="conversation_img" />
-                <div className="absolute">
-                    <h4>Our mission is simple</h4>
-                    <p>To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
+                <div className="absolute z-[20] space-y-3 -left-28 -bottom-8 bg-[#020043db] px-6 py-8 rounded-3xl w-[70%] text-white">
+                    <h4 className="text-2xl">Our mission is simple</h4>
+                    <p className="text-gray-400 text-sm">To provide high-quality healthcare services that are accessible, personalized, and patient-centered.</p>
                 </div>
             </div>
         </div>
